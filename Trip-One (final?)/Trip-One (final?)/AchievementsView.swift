b35f785f91660c095f7784201e5de86023f0f9b2
Ilfratommo <tommaso.pomi@gmail.com>
2024-12-17 10:49:30 +0100

    new version

---
+++ Trip-One (final?)/Trip-One (final?)/AchievementsView.swift
@@ -0,0 +1,18 @@
+//
+//  AchievementsView.swift
+//  Trip-One (final?)
+//
+//  Created by Tommaso D'Auria on 17/12/24.
+//
+
+import SwiftUI
+
+struct AchievementsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AchievementsView()
+}
---
+++ Trip-One (final?)/Trip-One (final?)/EditSubTextView.swift
@@ -0,0 +1,18 @@
+//
+//  EditSubTextView.swift
+//  Trip-One (final?)
+//
+//  Created by Tommaso D'Auria on 17/12/24.
+//
+
+import SwiftUI
+
+struct EditSubTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditSubTextView()
+}
